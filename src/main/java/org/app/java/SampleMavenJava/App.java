49f47c8e239b1c32d6ead2ac031f2bfb2fc1229b
ManeeshAvadhutha <avadhuthamaneesh7@gmail.com>
2025-12-03 19:41:22 +0530

    First commit

---
+++ src/main/java/org/app/java/SampleMavenJava/App.java
@@ -0,0 +1,13 @@
+package org.app.java.SampleMavenJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
